9197193d099e7d33bd4dcbad213272e475e682f0
Amresh Kumar Gond <145150083+amresh91620@users.noreply.github.com>
2026-06-12 07:31:53 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -17,19 +17,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://job-portal-kappa-coral.vercel.app',
-    process.env.FRONTEND_URL
-]
-
 app.use(cors({
     origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        callback(null, true)
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
